c17348634f9abfe4fbb5b745883db0f568bc522e
Everson <sousekjuniorr@outlook.com>
2023-12-05 00:45:23 -0300

    Estrutura de pastas e página 404 Feita

---
+++ routes/root.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+
+//Regex corresponde a raiz do site podendo receber um index com uma extensão opcional "".html"
+router.get("^/$|/index(.html)?"), (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'view', 'index.html'))
+}
+
+module.exports = router
